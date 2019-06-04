2365102e68a39bc8a4e7f66d62e466529c3c2f56
Taiye Salami <taiye41@gmail.com>
2019-06-04 18:03:51 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$("#text").append("this is so freaking cool.");
-//$(document).ready($("#text").append("this is so freaking cool.") {
 
-//})
+$(document).ready(function() {
+  $("#text").append("this is so freaking cool.");
+})
